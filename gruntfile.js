a116d423943968625353162fdc15bc54ae675f16
Anthony <zthun@icloud.com>
2017-04-18 07:08:10 -0500

    Added the travis build.

--- gruntfile.js
+++ gruntfile.js
@@ -2,52 +2,53 @@
 
 module.exports = function (grunt) {
     'use strict';
-    
+
     var dbg = grunt.option('dbg');
 
     require('load-grunt-tasks')(grunt);
-    
+
     var gruntConfig = {
         // Output paths
         paths: require('./config/gruntfile-config').paths,
-        
-        // Cleanup 
+
+        // Cleanup
         clean: require('./config/gruntfile-clean'),
-        
+
         // Checks
         jshint: require('./config/gruntfile-jshint'),
         bootlint: require('./config/gruntfile-bootlint'),
         htmlhint: require('./config/gruntfile-htmlhint'),
         sasslint: require('./config/gruntfile-sasslint'),
         karma: require('./config/gruntfile-karma'),
-        
+
         // Build
         ngtemplates: require('./config/gruntfile-ngtemplates'),
         uglify: require('./config/gruntfile-uglify'),
         sass: require('./config/gruntfile-sass'),
         copy: require('./config/gruntfile-copy')
     };
-    
+
     gruntConfig.uglify.options.sourceMap = dbg;
     gruntConfig.sass.options.sourceMap = dbg;
-    
+
     grunt.initConfig(gruntConfig);
-    
+
     grunt.registerTask('check', [
         'jshint',
         'bootlint',
         'htmlhint',
         'sasslint',
+        'travis-lint',
         'karma'
     ]);
-    
+
     grunt.registerTask('build', [
         'sass',
         'ngtemplates',
         'uglify',
         'copy'
     ]);
-    
+
     grunt.registerTask('default', [
         'clean',
         'check',
