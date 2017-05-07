0ff047f1804824bc4a421dd9500c84141ebba617
Anthony <zthun@icloud.com>
2017-05-07 14:11:20 -0500

    Fixed the uirouter path.

--- config/gruntfile-config.js
+++ config/gruntfile-config.js
@@ -12,7 +12,7 @@ module.exports = {
             'node_modules/jquery/dist/jquery.js',
             'node_modules/angular/angular.js',
             'node_modules/angular-animate/angular-animate.js',
-            'node_modules/angular-ui-router/release/angular-ui-router.js',
+            'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
             'node_modules/zpubsub/dist/zpubsub.all.js',
             'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
         ],
