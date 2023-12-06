a6cc1b4d24b8246fb05eaa175453dbf373d38096
Anthony Bonta <zthun.coding@gmail.com>
2023-12-06 17:31:39 -0600

    feat: clocks web is the evolution of the original ztimer app

---
+++ packages/clocks-web/src/app/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function ZClocksApp() {
+  return <div className='ZClocksApp-root'>Clocks!</div>;
+}
--- packages/clocks-web/src/index.tsx
+++ packages/clocks-web/src/index.tsx
@@ -1,1 +1,11 @@
-export const Placeholder = 0;
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ZClocksApp } from './app/app';
+
+const container = createRoot(document.getElementById('zthunworks-clocks')!);
+
+container.render(
+  <React.StrictMode>
+    <ZClocksApp />
+  </React.StrictMode>
+);
