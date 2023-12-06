0e405a708426830fdb19552a0cf8b83209788631
Anthony Bonta <zthun.coding@gmail.com>
2023-12-06 17:17:27 -0600

    refactor: structure

---
+++ packages/clock-legacy/app/ztimer.config.js
@@ -0,0 +1,31 @@
+/* global angular */
+(function () {
+  angular.module('ZTimer').config(ZTimerConfig);
+
+  ZTimerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function ZTimerConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('ztimer', {
+        abstract: true,
+        templateUrl: 'src/app/ztimer.html'
+      })
+      .state('ztimer.app', {
+        url: '/',
+        views: {
+          runner: {
+            templateUrl: 'src/app/runner/ztrunner.html',
+            controller: 'ZtRunnerController',
+            controllerAs: 'runner'
+          },
+          operations: {
+            templateUrl: 'src/app/operations/ztoperations.html',
+            controller: 'ZtOperationsController',
+            controllerAs: 'operations'
+          }
+        }
+      });
+  }
+})();
