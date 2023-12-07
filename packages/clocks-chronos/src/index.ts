119bb002ca8ec071a55f9f0269b46005db167064
Anthony Bonta <zthun.coding@gmail.com>
2023-12-07 16:00:56 -0600

    core: export

--- packages/clocks-chronos/src/index.ts
+++ packages/clocks-chronos/src/index.ts
@@ -1,5 +1,5 @@
 export * from './culture/culture.mjs';
-export * from './format/format.mjs';
+export * from './format/format-date.mjs';
 export * from './guess/guess-date.mjs';
 export * from './parse/parse-date.mjs';
 export * from './timezone/timezone.mjs';
