7562b6f3cd6a2caaac1e99ab44a8a1657e7a42dc
Anthony Bonta <zthun.coding@gmail.com>
2023-12-11 08:49:21 -0600

    build: vite configuration

---
+++ packages/clocks-react/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index'
+    }
+  },
+  plugins: [externalizeDeps(), dts({ exclude: ['**/*.spec.ts'] }), tsconfigPaths()]
+});
