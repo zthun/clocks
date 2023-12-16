950d34d2f6d2c335ef2100a8cbaad605d4ed0beb
Anthony Bonta <zthun.coding@gmail.com>
2023-12-16 09:51:15 -0600

    feat: you can select the format and culture for dates

--- packages/clocks-web/src/time-zone/time-zone-page.cm.ts
+++ packages/clocks-web/src/time-zone/time-zone-page.cm.ts
@@ -1,10 +1,28 @@
 import { ZCircusBy, ZCircusComponentModel } from '@zthun/cirque';
 import { ZClockComponentModel } from '@zthun/clocks-dom';
+import { ZChoiceComponentModel } from '@zthun/fashion-boutique';
 
 export class ZTimeZonePageComponentModel extends ZCircusComponentModel {
   public static readonly Selector = '.ZTimeZonePage-root';
 
   public analog(): Promise<ZClockComponentModel> {
     return ZCircusBy.first(this.driver, ZClockComponentModel, 'analog');
+  }
+
+  public async value(): Promise<string> {
+    const value = await this.driver.select('.ZTimeZonePage-value');
+    return value.text();
+  }
+
+  public async culture(): Promise<ZChoiceComponentModel> {
+    return ZCircusBy.first(this.driver, ZChoiceComponentModel, 'culture');
+  }
+
+  public async format(): Promise<ZChoiceComponentModel> {
+    return ZCircusBy.first(this.driver, ZChoiceComponentModel, 'format');
+  }
+
+  public async timeZone(): Promise<ZChoiceComponentModel> {
+    return ZCircusBy.first(this.driver, ZChoiceComponentModel, 'time-zone');
   }
 }
--- packages/clocks-web/src/time-zone/time-zone-page.tsx
+++ packages/clocks-web/src/time-zone/time-zone-page.tsx
@@ -1,9 +1,9 @@
-import { formatDateTime, timeZones, userTimeZone } from '@zthun/clocks-fn';
+import { ZDateFormats, culture, cultures, formatDateTime, timeZones, userTimeZone } from '@zthun/clocks-fn';
 import { ZClockAnalog, useDate } from '@zthun/clocks-react';
 import { ZBox, ZCard, ZChoiceAutocomplete, ZH2, ZStack } from '@zthun/fashion-boutique';
 import { ZSizeFixed } from '@zthun/fashion-tailor';
 import { ZOrientation, setFirst } from '@zthun/helpful-fn';
-import { identity } from 'lodash';
+import { identity } from 'lodash-es';
 import React, { useMemo, useState } from 'react';
 
 export function ZTimeZonePage() {
@@ -12,8 +12,17 @@ export function ZTimeZonePage() {
   const _timeZone = useMemo(() => [timeZone], [timeZone]);
   const _setTimeZone = setFirst.bind(null, setTimeZone, userTimeZone());
   const _timeZones = useMemo(() => timeZones(), []);
+  const [format, setFormat] = useState<string>(ZDateFormats.LocalDateTime);
+  const _format = useMemo(() => [format], [format]);
+  const _setFormat = setFirst.bind(null, setFormat, ZDateFormats.LocalDateTime);
+  const _formats = Object.values(ZDateFormats);
+  const [locale, setLocale] = useState<string>(culture());
+  const _culture = useMemo(() => [locale], [locale]);
+  const _setCulture = setFirst.bind(null, setLocale, culture());
+  const _cultures = cultures();
 
-  const subHeading = formatDateTime(current, { timeZone, format: 'xxx' });
+  const subHeading = formatDateTime(current, { timeZone, format: `'UTC' xxx` });
+  const formatted = formatDateTime(current, { timeZone, format, culture: locale });
 
   return (
     <ZCard className='ZTimeZonePage-root' heading={timeZone} subHeading={subHeading}>
@@ -22,14 +31,39 @@ export function ZTimeZonePage() {
           <ZClockAnalog value={current} timeZone={timeZone} name='analog' />
         </ZBox>
 
-        <ZH2>Options</ZH2>
+        <ZH2 className='ZTimeZonePage-value' compact>
+          {formatted}
+        </ZH2>
+
+        <ZH2 compact>Options</ZH2>
 
         <ZChoiceAutocomplete
           label='Time Zone'
           name='time-zone'
           value={_timeZone}
+          indelible
           onValueChange={_setTimeZone}
           options={_timeZones}
+          identifier={identity}
+        />
+
+        <ZChoiceAutocomplete
+          label='Culture'
+          name='culture'
+          value={_culture}
+          indelible
+          onValueChange={_setCulture}
+          options={_cultures}
+          identifier={identity}
+        />
+
+        <ZChoiceAutocomplete
+          label='Format'
+          name='format'
+          value={_format}
+          indelible
+          onValueChange={_setFormat}
+          options={_formats}
           identifier={identity}
         />
       </ZStack>
